1fdace0f9a5dacd1150c3c649c12f772ac3c36dc
Lennart401 <lennart.popkes@gmail.com>
2020-05-19 17:15:51 +0200

    Test production build

--- build/precache-manifest.3d62269a32c78cdc8156bd9924f914bc.js
+++ build/precache-manifest.3d62269a32c78cdc8156bd9924f914bc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5cdfdf640d7f356f28d86531fd40b1f",
+    "revision": "700b4935a41e4dda9b2d6bc1e8ab1eda",
     "url": "/quotracker/index.html"
   },
   {
-    "revision": "facdd5bef635232f3632",
+    "revision": "42c8ad8e5466afe06f7e",
     "url": "/quotracker/static/css/main.797c6ce9.chunk.css"
   },
   {
-    "revision": "c84c64a3e63eff4e1558",
-    "url": "/quotracker/static/js/2.8ed197d8.chunk.js"
+    "revision": "0be11a8fe46a32f8025f",
+    "url": "/quotracker/static/js/2.29c679b6.chunk.js"
   },
   {
     "revision": "0f4e465155d5cc7d6bc82b3f8e0a033e",
-    "url": "/quotracker/static/js/2.8ed197d8.chunk.js.LICENSE.txt"
+    "url": "/quotracker/static/js/2.29c679b6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "facdd5bef635232f3632",
-    "url": "/quotracker/static/js/main.411d735f.chunk.js"
+    "revision": "42c8ad8e5466afe06f7e",
+    "url": "/quotracker/static/js/main.e0d874e3.chunk.js"
   },
   {
     "revision": "26e39b57e911d460b543",

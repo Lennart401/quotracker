57f9f10b08c64d3bc6d1ed9a846946ef93ff74ba
Lennart401 <lennart.popkes@gmail.com>
2020-05-20 23:23:55 +0200

    Added build from Version 0.6.11

---
+++ build/precache-manifest.2c8e2123efeb8bc1a28db249292707b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ae9201ff2ce99b6d65565535f7b678",
+    "url": "/quotracker/index.html"
+  },
+  {
+    "revision": "e9441bfedd0b1cce2deb",
+    "url": "/quotracker/static/css/main.797c6ce9.chunk.css"
+  },
+  {
+    "revision": "0be11a8fe46a32f8025f",
+    "url": "/quotracker/static/js/2.29c679b6.chunk.js"
+  },
+  {
+    "revision": "0f4e465155d5cc7d6bc82b3f8e0a033e",
+    "url": "/quotracker/static/js/2.29c679b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9441bfedd0b1cce2deb",
+    "url": "/quotracker/static/js/main.8f3ec5e8.chunk.js"
+  },
+  {
+    "revision": "26e39b57e911d460b543",
+    "url": "/quotracker/static/js/runtime-main.95fd54b3.js"
+  }
+]);
